e687a188b66cf8f74753ffe150c5792d78a97c4a
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-24 20:42:04 -0500

    add show, update, delete actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,8 +10,44 @@ class JobsController < ApplicationController
     }
   end
 
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      render :show
+    else
+      render json: {error: @job.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+  
   def index
     @jobs = Job.all
     render :index
+  end
+  
+  def show
+    @job = Job.find_by(id: params[:id])
+    render :show
+  end
+  
+  def update
+    @job = Job.find_by(id: params[:id])
+    @job.id = params[:id] || @job.id,
+    @job.title = params[:title] || @job.title,
+    @job.date_time = params[:date_time] || @job.date_time,
+    @job.duration = params[:duration] || @job.duration,
+    @job.details = params[:details] || @job.details,
+    @job.user_id = params[:user_id] || @job.user_id,
+
+    if @job.save
+      render :show
+    else
+      render json: {error: @job.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @job = Job.find_by(id: params[:id])
+    @job.destroy
+    render json: {message: "#{@job.title} job successfully deleted"}
   end
 end
